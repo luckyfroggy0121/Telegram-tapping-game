78d503408cc759b2067d8dcd013061ed31c0eb2e
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2024-07-02 03:39:28 +0200

    refactor: Update image imports to use SVG instead of PNG

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -26,7 +26,7 @@ const DrawerOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DrawerPrimitive.Overlay
     ref={ref}
-    className={cn("fixed inset-0 z-50 bg-black/80", className)}
+    className={cn("fixed inset-0 z-50 bg-white/25", className)}
     {...props}
   />
 ));
@@ -41,12 +41,11 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border bg-background",
+        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] [box-shadow:3px_-10px_26.2px_0px_#00000026] bg-[#9e7ed3]",
         className
       )}
       {...props}
-    >
-      <div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-muted" />
+    >   
       {children}
     </DrawerPrimitive.Content>
   </DrawerPortal>
--- src/pages/JoinTank.tsx
+++ src/pages/JoinTank.tsx
@@ -9,7 +9,15 @@ import mexc from "@/assets/images/mexc.png";
 import binanceMedal from "@/assets/svg/binance-medal.svg?react";
 import bybitMedal from "@/assets/svg/bybit-medal.svg?react";
 import okxxMedal from "@/assets/svg/okxx-medal.svg?react";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerTrigger,
+  DrawerTitle,
+} from "@/components/ui/drawer";
+import { Button } from "@/components/ui/button";
+import { IoCloseCircleSharp } from "react-icons/io5";
 
 const tanks = [
   {
@@ -55,24 +63,39 @@ const JoinTank = () => {
       <div className="text-white/80 font-bold text-[16px] text-center mt-3 max-w-[18rem]">
         Which tank do you want to join and see us being listed on?
       </div>
-      <ScrollArea className="rounded-[11px] mt-2 w-full bg-[#C3C3C340]">
+      <div className="rounded-[11px] mt-2 w-full bg-[#C3C3C340]">
         {tanks.map(({ name, image, Medal }, index) => (
-          <div
-            key={index}
-            className="flex items-center gap-2 p-3 justify-between"
-          >
-            <div className="flex items-center gap-2">
-              <img src={image} alt={name} className="h-10" />
-              <div className="font-bold text-white text-[15px]">{name}</div>
-            </div>
-            {Medal ? (
-              <Medal className="h-5 w-5" />
-            ) : (
-              <div className="text-white">{index + 1}</div>
-            )}
-          </div>
+          <Drawer key={index}>
+            <DrawerTrigger className="flex items-center gap-2 p-3 justify-between w-full">
+              <div className="flex items-center gap-2">
+                <img src={image} alt={name} className="h-10" />
+                <div className="font-bold text-white text-[15px]">{name}</div>
+              </div>
+              {Medal ? (
+                <Medal className="h-5 w-5" />
+              ) : (
+                <div className="text-white">{index + 1}</div>
+              )}
+            </DrawerTrigger>
+            <DrawerContent className="flex pt-7 pb-8 flex-col items-center">
+              <DrawerTitle className="ml-auto mr-5">
+                <DrawerClose>
+                  <IoCloseCircleSharp color="#FFFFFF80"  size={25}/>
+                </DrawerClose>
+              </DrawerTitle>
+              <img src={image} alt={name} className='w-[100px]' />
+              <div className="font-bold text-[24px] leading-[18px] text-white my-6">{name}</div>
+              <Button
+                onClick={() => {}}
+                className="w-[250px] bg-[#9712F4] h-[48px] font-bold text-[16px] leading-5 rounded-[30px]"
+                style={{ boxShadow: "0px 4px 4px 0px #00000040" }}
+              >
+                Join Tank
+              </Button>
+            </DrawerContent>
+          </Drawer>
         ))}
-      </ScrollArea>
+      </div>
     </div>
   );
 };
