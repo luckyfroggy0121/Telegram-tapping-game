d3d5c342fc22667f20a5c3f7db8b42bfd7ac28b7
NineTeen <luckyfroggy0121@gmail.com>
2024-07-14 06:37:31 -0400

    fix err 1

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import "./index.css";
 import App from "./App";
 
