49c7bb6c7815dcb4e6d68bfc11789e80db502ee6
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2024-07-19 17:31:51 +0200

    [fix] fixed eslint stuffs

--- src/context/UserProvider.tsx
+++ src/context/UserProvider.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { User } from "@/interface/User";
 import LoadingPage from "@/pages/Loading";
 import { ReactNode, createContext, useEffect, useState } from "react";
--- src/layout/GameLayout.tsx
+++ src/layout/GameLayout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import HomePage from "@/pages/Home";
 import Navbar from "../components/common/Navbar";
 import { Toaster } from "react-hot-toast";
