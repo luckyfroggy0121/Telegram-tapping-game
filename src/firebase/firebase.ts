24cb749a77c7cc5b7e9c4a61a18ba7f1508727b5
NineTeen <luckyfroggy0121@gmail.com>
2024-07-23 19:23:37 -0400

    firebase on lucky

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -17,13 +17,13 @@ import {getFirestore} from "firebase/firestore"
 // };
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCS8vjr-DS6-HG5xe6l2gE35fhj3o6JBc0",
-  authDomain: "carbide-ratio-429520-a1.firebaseapp.com",
-  projectId: "carbide-ratio-429520-a1",
-  storageBucket: "carbide-ratio-429520-a1.appspot.com",
-  messagingSenderId: "696995656499",
-  appId: "1:696995656499:web:79d7e97b414da9edaf8446",
-  measurementId: "G-RKVBGL8PW1"
+  apiKey: "AIzaSyAwmohNC33lZ-ZKGrNicFoM0vPbdR--cGo",
+  authDomain: "daniel-smartlitre.firebaseapp.com",
+  projectId: "daniel-smartlitre",
+  storageBucket: "daniel-smartlitre.appspot.com",
+  messagingSenderId: "1090634472803",
+  appId: "1:1090634472803:web:16ccf7b5d6192ac667c756",
+  measurementId: "G-MVHYJMLFBD"
 };
 
 // Initialize Firebase
