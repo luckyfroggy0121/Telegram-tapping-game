292f6808ff912ba74f372eea9a8692139477bfe5
NineTeen <luckyfroggy0121@gmail.com>
2024-07-23 04:52:57 -0400

    firebase changes for handover

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -4,23 +4,26 @@ import {GoogleAuthProvider,getAuth,signInWithPopup} from "firebase/auth";
 import {getFirestore} from "firebase/firestore"
 
 
+
+
 // const firebaseConfig = {
-//   apiKey: "AIzaSyCUnzC51OaM_qIYk6hVJ3iQpawNKjkyPHw",
-//   authDomain: "smartlitre.firebaseapp.com",
-//   projectId: "smartlitre",
-//   storageBucket: "smartlitre.appspot.com",
-//   messagingSenderId: "171653683994",
-//   appId: "1:171653683994:web:2ae12a51f0c9bcb50beaa3"
+//   apiKey: "AIzaSyAwmohNC33lZ-ZKGrNicFoM0vPbdR--cGo",
+//   authDomain: "daniel-smartlitre.firebaseapp.com",
+//   projectId: "daniel-smartlitre",
+//   storageBucket: "daniel-smartlitre.appspot.com",
+//   messagingSenderId: "1090634472803",
+//   appId: "1:1090634472803:web:49238ed1cb68bc6067c756",
+//   measurementId: "G-R8YY93X9N2"
 // };
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAwmohNC33lZ-ZKGrNicFoM0vPbdR--cGo",
-  authDomain: "daniel-smartlitre.firebaseapp.com",
-  projectId: "daniel-smartlitre",
-  storageBucket: "daniel-smartlitre.appspot.com",
-  messagingSenderId: "1090634472803",
-  appId: "1:1090634472803:web:49238ed1cb68bc6067c756",
-  measurementId: "G-R8YY93X9N2"
+  apiKey: "AIzaSyCS8vjr-DS6-HG5xe6l2gE35fhj3o6JBc0",
+  authDomain: "carbide-ratio-429520-a1.firebaseapp.com",
+  projectId: "carbide-ratio-429520-a1",
+  storageBucket: "carbide-ratio-429520-a1.appspot.com",
+  messagingSenderId: "696995656499",
+  appId: "1:696995656499:web:79d7e97b414da9edaf8446",
+  measurementId: "G-RKVBGL8PW1"
 };
 
 // Initialize Firebase
